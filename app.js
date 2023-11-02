38b2a726e77ec558c6b6b481f7f1639a7dc37630
mr-Bones3108 <nikhil788p@gmail.com>
2023-11-02 16:50:53 +0530

    create a board module

--- app.js
+++ app.js
@@ -1,55 +1,40 @@
-const createGameBoard = ()=>{
-  const board =  [["X", "O", "O"], ["O", "X", "X"], ["X", "O", "X"]];
-  return {
-      board,
-  };
-};
-
 const container = document.getElementById('container')
 
+//BoardModule
+const createGameBoard = (() => {
+  let board = [["", "", ""], ["", "", ""], ["", "", ""]];
+  const container = document.getElementById("container"); // Assuming you have a container element with the id "container"
 
-const playerModule =(()=>{
-  const createPlayer = (name,sign)=>{
-      const getName = ()=>{
-          return name
-      };
-      const getSign = ()=>{
-          return sign
-      };
-
-      return {
-          getName,
-          getSign
-      };
+  const createBoard = () => {
+    board.forEach((row) => {
+      row.forEach((element) => {
+        const boxDiv = document.createElement('div');
+        boxDiv.classList = 'square';
+        boxDiv.textContent = element;
+        container.appendChild(boxDiv);
+      });
+    });
   };
-
-  const player1 = createPlayer("Player1", "X")
-  const player2 = createPlayer("Player2", "O")
 
   return {
-      player1,
-      player2
+    createBoard,
   };
-
 })();
 
-playerModule.player1.getName()
-playerModule.player1.getSign()
+createGameBoard.createBoard()
+
+//factory function
+const createPlayer = (name,mark)=>{
+    return{
+      name,
+      mark
+    }
+};
 
 
-playerModule.player2.getName()
-playerModule.player2.getSign()
 
 
-const gameBoard = createGameBoard()
-console.log(gameBoard)
 
 
-gameBoard.board.forEach(element => {
-  element.forEach(element =>{
-      const boxDiv = document.createElement('div');
-      boxDiv.classList = 'square';
-      boxDiv.textContent = element
-      container.appendChild(boxDiv)
-  })
-});
+
+
