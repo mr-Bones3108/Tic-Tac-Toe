8f05d600379fa554923aef0cc89e3acc81e5c209
mr-Bones3108 <nikhil788p@gmail.com>
2023-11-03 12:00:49 +0530

    add a logic to check for win or tie

--- app.js
+++ app.js
@@ -23,11 +23,19 @@ const createGameBoard = (() => {
     const getGameBoard =()=>{
       return board
     }
+
+    const reset =()=>{
+      for(let i=0; i<board.length;i++){
+        board[i]=""
+      }
+      createBoard()
+    }
   
   return {
     createBoard,
     update,
-    getGameBoard
+    getGameBoard,
+    reset
   };
 })();
 
@@ -54,28 +62,86 @@ const game = (()=>{
       createPlayer(document.querySelector("#player2").value, "O")
     ]
     currPlayer=0;
+    gameOver=false;
     createGameBoard.createBoard()
+    const squares = document.querySelectorAll(".square");
+    squares.forEach((square) => {
+      square.addEventListener("click", game.handleClick);
+    });
   };
 
   const handleClick = (event)=>{
+    if(gameOver){
+      return;
+    }
     const index = parseInt(event.target.id.split("-")[1])
     const valuePresent = createGameBoard.getGameBoard()[index];
     if (valuePresent !== ""){
       return;
     }
     createGameBoard.update(index,players[currPlayer].mark)
-    currPlayer = currPlayer === 0 ? 1 : 0;
 
+    if(checkForWin(createGameBoard.getGameBoard(), players[currPlayer].mark)){
+      gameOver=true;
+      alert(`${players[currPlayer].name} won!`)
+    }
+    else if(checkForTie(createGameBoard.getGameBoard())){
+      gameOver=true;
+      alert(`Its a tie`) 
+    }
+    // console.log(createGameBoard.getGameBoard())
+    currPlayer = currPlayer === 0 ? 1 : 0;
+  }
+
+  const restart = ()=>{
+    createGameBoard.reset()
+    // console.log(createGameBoard.getGameBoard())
   }
 
   return{
     start,
-    handleClick
+    handleClick,
+    restart
   }
 })();
 
+
+function checkForWin(board){
+  const winningCombinations = [
+    // Horizontal Lines (Rows)
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  
+    // Vertical Lines (Columns)
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  
+    // Diagonals
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for(let i=0 ; i<winningCombinations.length;i++){
+    const [a,b,c] = winningCombinations[i]
+    if(board[a] && board[a]===board[b] && board[a]===board[c]){
+      return true;
+    }
+  }
+  return false
+}
+
+function checkForTie(board){
+  return board.every(cell => cell !=="")
+}
+
 function playGame(){
   game.start()
+}
+
+function restartPlay(){
+  game.restart()
 }
 
 
